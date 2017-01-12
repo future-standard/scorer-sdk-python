317ae0ebc7dda88abdad79f8832231cd8494046a
Mikihisa Hayashi <mikihisa.hayashi@futurestandard.co.jp>
2017-01-12 16:29:19 +0900

    First Commit

---
+++ scorer/__init__.py
@@ -0,0 +1,3 @@
+from scorer.sdk import Scorer
+
+__all__ = ('Scorer')
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+def main():
+    setup(
+        name='scorer-sdk',
+        version='0.4.0',
+        description='A Software Development Kit for Scorer',
+        url='https://github.com/future-standard/',
+        zip_safe=False,
+        packages=['scorer'],
+    )
+
+
+if __name__ == '__main__':
+    main()
