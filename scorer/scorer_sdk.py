317ef5010617137505a177ca9f99e419a54b468b
Mikihisa Hayashi <mikihisa.hayashi@futurestandard.co.jp>
2017-01-18 14:03:08 +0900

    Change api structure

--- scorer/scorer_sdk.py
+++ scorer/scorer_sdk.py
@@ -28,20 +28,198 @@ from datetime import datetime
 import pickle
 
 #ZMQ scoket String
-img_sock      = "ipc://@/scorer/frame_grabber-video0"
-log_sock      = "ipc://@/scorer/logger-marker"
-sdk_sock      = "ipc://@/scorer/logger-sdk"
-web_sock1      = "ipc://@/scorer/web-sdk1"
-web_sock2      = "ipc://@/scorer/web-sdk2"
-web_sock3      = "ipc://@/scorer/web-sdk3"
-web_sock4      = "ipc://@/scorer/web-sdk4"
+img_sock_str      = "ipc://@/scorer/frame_grabber-video0"
+sdk_sock_str      = "ipc://@/scorer/logger-sdk"
+web_sock1_str     = "ipc://@/scorer/web-sdk1"
+web_sock2_str     = "ipc://@/scorer/web-sdk2"
+web_sock3_str     = "ipc://@/scorer/web-sdk3"
+web_sock4_str     = "ipc://@/scorer/web-sdk4"
 MAX_WEB_SCOCK  = 4
 
 SCORER_DEV_CONFIG = "/opt/scorer/home/dev/lib/CONFIG"
 USER_ROI_DATA = "USER_ROI_DATA="
 
-class Frame:
-    """Frame class. This class handles frame and date data.
+# For ZMQ connetion
+ctx = zmq.Context()
+
+sdk_sock = ctx.socket(zmq.PUB)
+sdk_sock.setsockopt(zmq.SNDHWM, 10000)
+sdk_sock.connect(sdk_sock_str)
+
+task = "SDK"
+
+# For web_show
+web_sock1 = ctx.socket(zmq.PUB)
+web_sock1.setsockopt(zmq.SNDHWM, 10000)
+#
+web_sock2 = ctx.socket(zmq.PUB)
+web_sock2.setsockopt(zmq.SNDHWM, 10000)
+#
+web_sock3 = ctx.socket(zmq.PUB)
+web_sock3.setsockopt(zmq.SNDHWM, 10000)
+#
+web_sock4 = ctx.socket(zmq.PUB)
+web_sock4.setsockopt(zmq.SNDHWM, 10000)
+#
+web_sock1_bind_set = False
+web_sock2_bind_set = False
+web_sock3_bind_set = False
+web_sock4_bind_set = False
+
+
+def imshow(index, cvmat):
+    """Send the opencv image data to Flask web server
+
+    :param cvdata: Opencv image data
+    :param index: Web server index
+    """
+    serialized = pickle.dumps(cvdata, protocol=4)
+    if index == 1:
+        if web_sock1_bind_set == False:
+            web_sock1_bind_set = True
+            web_sock1.bind(web_sock1_str)
+
+        web_sock1.send(serialized)
+    elif index == 2:
+        if web_sock2_bind_set == False:
+            web_sock2_bind_set = True
+            web_sock2.bind(web_sock2_str)
+
+        web_sock2.send(serialized)
+    elif index == 3:
+        if web_sock3_bind_set == False:
+            web_sock3_bind_set = True
+            web_sock3.bind(web_sock3_str)
+
+        web_sock3.send(serialized)
+    elif index == 4:
+        if web_sock4_bind_set == False:
+            web_sock4_bind_set = True
+            web_sock4.bind(web_sock4_str)
+
+        web_sock4.send(serialized)
+    else:
+        print("the index[" + index + "] is invalid")
+        return False
+    return True
+
+def upload(log_dictionary=None, log_list=None, log_str=None, images=None):
+    """upload Upload snapshots image and log
+
+    :param log_dictionary: Dictionary
+    :param log_list: List
+    :param log_str: String
+    :param images: list of numpy images
+    :return: upload success then retun True otherwise return False
+    """
+    result_str=""
+
+    # Syntax check
+    if log_dictionary is not None and isinstance(log_dictionary, dict) == False:
+         print("log_dictionary is not a dictionary")
+         return False
+
+    if log_list is not None and isinstance(log_list, list) == False:
+         print("log_list is not a list")
+         return False
+
+    if log_str is not None and isinstance(log_str, str) == False:
+         print("log_str is not a String")
+         return False
+
+    if images is not None and isinstance(images, list) == False:
+         print("images is not a list")
+         return False
+
+    if images is not None and len(images) >= 6:
+        print("images list len must be less than 5")
+        return False;
+
+    # Combine the log payload text
+    if log_dictionary is not None:
+        result_str=str(log_dictionary)
+    if log_list is not None:
+        result_str=result_str+str(log_list)
+    if log_str is not None:
+        result_str=result_str + "(" + log_str + ")"
+
+    result_str = result_str.replace(' ', '')
+    if len(result_str) == 0:
+        result_str="\"\""
+
+    name = "Log".encode('utf-8')
+    id = "1".encode('utf-8')
+
+    images_n = len(images) if images is not None else 0
+
+    data = [ name, id, task.encode('utf-8'), result_str.encode('utf-8'), np.array( [images_n] ) ]
+    sdk_sock.send_multipart(data, zmq.SNDMORE )
+
+    # If no images, just send log payload
+    if images_n == 0:
+            sdk_sock.send_multipart(data)
+            return True
+
+    # Send images and log payload
+    for i in range( images_n ):
+        height, width = images[i].shape[:2]
+        ndim = images[i].ndim
+        data_info = np.array( [height, width, ndim] );
+
+        data = [ data_info, images[i].data ]
+        if i == len(images) -1:
+            sdk_sock.send_multipart(data)
+        else:
+            sdk_sock.send_multipart(data, zmq.SNDMORE)
+
+    return True
+
+class VideoCapture:
+    """VideoCapture class. Class for video captureing from camera.
+    """
+    def __init__(self, index, blocking=True):
+        zmq_str = "ipc://@/scorer/frame_grabber-video"
+        sock = zmq_str + str(index);
+        #
+        self.img_sock = ctx.socket(zmq.SUB)
+        self.img_sock.setsockopt_string(zmq.SUBSCRIBE, '')
+        self.img_sock.setsockopt(zmq.RCVHWM, 1)
+        self.img_sock.connect(img_sock)
+        #
+        self.poller = zmq.Poller()
+        self.poller.register(self.img_sock, zmq.POLLIN)
+        #a
+        self.blocking=blocking
+        if(blocking == True):
+            self.timeout = None
+        else:
+            self.timeout = 0
+
+    def read(self):
+        self.events =  dict(self.poller.poll(self.timeout))
+        self.count = 0
+        try:
+            while True:
+                socks = self.events
+                if self.img_sock in socks and socks[self.img_sock] == zmq.POLLIN:
+                    topic, id, timestamp, my_type, format, rows, cols, mat_type, data = \
+                                             self.img_sock.recv_multipart(zmq.NOBLOCK, True, False)
+                    self.frame = VideoFrame(timestamp, format, rows, cols, mat_type, data)
+                    self.count = 1
+        except:
+            if self.count == 0:
+                return (None)
+        return (self.frame)
+
+
+    def isOpend(self):
+        return self.img_sock.closed
+
+    def release(self):
+        self.img_sock.close()
+
+class VideoFrame:
+    """VideoFrame class. This class handles frame and date data.
     """
     def __init__(self, timestamp, format, rows, cols, mat_type, data):
         """Initialize the instance
@@ -60,12 +238,14 @@ class Frame:
         self.image = np.frombuffer(data, dtype=np.uint8).reshape((self.my_row[0],self.my_col[0]));
         self.image_format=format.decode('utf-8')
 
-    def get_image():
-        """Get raw image data from camera
+        epoch_time = self.my_time[0]/1000000
+        epoch_msec = self.my_time[0]%1000000
 
-        :return: raw image data
-        """
-        return self.image
+        self.width = self.my_col[0]
+        self.height = self.my_row[0]
+        self.time = self.my_time[0]
+        self.datetime= datetime(*time.localtime(epoch_time)[:6])
+        self.msec= epoch_msec
 
     def get_bgr(self):
         """ Get bgr image data
@@ -96,20 +276,6 @@ class Frame:
         else:
             raise Exception("format is incorrect")
         return gray
-
-    def get_col(self):
-        """Get column number
-
-        :return: column number of this image
-        """
-        return self.my_col[0]
-
-    def get_row(self):
-        """Get row number
-
-        :return: row number of this image
-        """
-        return self.my_row[0]
 
     def get_frame_time(self):
         """Get frame timestamp
@@ -154,10 +320,6 @@ class Scorer:
         self.img_sock.setsockopt_string(zmq.SUBSCRIBE, '')
         self.img_sock.setsockopt(zmq.RCVHWM, 1)
         self.img_sock.connect(img_sock)
-        #
-        self.log_sock = ctx.socket(zmq.PUB)
-        self.log_sock.setsockopt(zmq.SNDHWM, 10000)
-        self.log_sock.connect(log_sock)
         #
         self.sdk_sock = ctx.socket(zmq.PUB)
         self.sdk_sock.setsockopt(zmq.SNDHWM, 10000)
@@ -347,7 +509,7 @@ class Scorer:
                 if self.img_sock in socks and socks[self.img_sock] == zmq.POLLIN:
                     topic, id, timestamp, my_type, format, rows, cols, mat_type, data = \
                                              self.img_sock.recv_multipart(zmq.NOBLOCK, True, False)
-                    self.frame = Frame(timestamp, format, rows, cols, mat_type, data)
+                    self.frame = VideoFrame(timestamp, format, rows, cols, mat_type, data)
                     self.count = 1
         except:
             if self.count == 0:
