c2671d397496070932383c4755c04e2380c42d71
Yusuke Nakano <yusuke.nakano@futurestandard.co.jp>
2018-05-25 12:51:31 +0900

    Add LogReceive class

--- scorer/scorer_sdk.py
+++ scorer/scorer_sdk.py
@@ -315,3 +315,60 @@ class VideoFrame:
         else:
             raise Exception("format is incorrect")
         return gray
+
+class LogReceive:
+    """LogReceive class. Class for log receiving.
+    """
+    def __init__(self, blocking=True):
+        """Initialize the instance
+        :param blocking: True if LogReceive read data as blocking mode
+
+        """
+        sock = "ipc://@/scorer/tracker-sdk"
+        #
+        self.log_sock = ctx.socket(zmq.SUB)
+        self.log_sock.setsockopt_string(zmq.SUBSCRIBE, '')
+        self.log_sock.setsockopt(zmq.RCVHWM, 1)
+        self.log_sock.bind(sock)
+        #
+        self.poller = zmq.Poller()
+        self.poller.register(self.log_sock, zmq.POLLIN)
+        #a
+        self.blocking=blocking
+        if(blocking == True):
+            self.timeout = None
+        else:
+            self.timeout = 0
+
+    def get(self):
+        """ Get data from LogReceive
+
+        :return: Log data
+        """
+        self.events =  dict(self.poller.poll(self.timeout))
+        self.count = 0
+        try:
+            while True:
+                socks = self.events
+                if self.log_sock in socks and socks[self.log_sock] == zmq.POLLIN:
+                    topic, id, log = self.log_sock.recv_multipart(zmq.NOBLOCK, True, False)
+                    self.log_data = LogData(log)
+                    self.count = 1
+        except:
+            if self.count == 0:
+                return (None)
+        return (self.log_data)
+
+
+    def isOpend(self):
+        """ Return true if LogReceive has been ready to read
+
+        :return: True of False
+        """
+        return not(self.log_sock.closed)
+
+    def release(self):
+        """ Close log receiving connection
+
+        """
+        self.log_sock.close()
